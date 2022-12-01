5e9f60b8cd14f18673411438b2128c36b496c930
Vishnu <vishnuvichuz198@gmail.com>
2022-12-01 18:28:17 +0530

    cors set

--- api/index.js
+++ api/index.js
@@ -30,7 +30,7 @@ app.use((req, res, next) => {
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://www.prosper-media.cf",
   })
 );
 app.use(cookieParser());
